eb62518447e999ccea15a5be138e2a337f2ab9fe
BugHunter-X <askzenai@gmail.com>
2024-12-20 08:44:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo `Camera` API with certain Android devices.  The camera preview may freeze or become unresponsive after a period of use, requiring the app to be restarted. This is particularly noticeable when using the camera for extended periods or recording video.
